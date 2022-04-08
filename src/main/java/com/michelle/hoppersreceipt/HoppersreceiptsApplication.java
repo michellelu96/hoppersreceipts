07a1c1a684fcd62327724bc5af6ab218b611b765
michellelu96 <lu.michelle96@icloud.com>
2022-04-08 14:27:51 -0700

    Initial commit

---
+++ src/main/java/com/michelle/hoppersreceipt/HoppersreceiptsApplication.java
@@ -0,0 +1,13 @@
+package com.michelle.hoppersreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersreceiptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersreceiptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelle/hoppersreceipt/HoppersreceiptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelle.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersreceiptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
